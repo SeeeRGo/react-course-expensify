8dde934843eec730cf9a45fe8b6e656ca1d31ff2
SeeeRGo <seeergo@email.com>
2018-03-16 20:20:12 +0400

    Add private routes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom'
-import AppRouter from './routers/AppRouter'
+import AppRouter, { history } from './routers/AppRouter'
 import configureStore from './store/configureStore'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
@@ -10,20 +10,40 @@ import 'react-dates/lib/css/_datepicker.css'
 
 import getVisibleExpenses from './selectors/expenses'
 import { startSetExpenses } from './actions/expenses'
-import {setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate} from './actions/filters'
-import './firebase/firebase'
+import { login, logout } from './actions/auth'
+import { firebase } from './firebase/firebase'
 
 const store = configureStore()
 
 const jsx = (
-		<Provider store={store}>
-			<AppRouter/>
-		</Provider>
-	)
+	<Provider store={store}>
+		<AppRouter/>
+	</Provider>
+)
+let hasRendered = false
+const renderApp = () => {
+	if(!hasRendered) {
+		ReactDOM.render(jsx, document.getElementById("app"))
+		hasRendered = true
+	}
+}
 
 ReactDOM.render(<p>Loading...</p>, document.getElementById("app"))
 
-store.dispatch(startSetExpenses()).then(() => {
-	ReactDOM.render(jsx, document.getElementById("app"))
-})
 
+
+firebase.auth().onAuthStateChanged((user) => {
+	if(user) {
+		store.dispatch(login(user.uid))
+		store.dispatch(startSetExpenses()).then(() => {
+			renderApp()
+			if(history.location.pathname === '/') {
+				history.push('/dashboard')
+			}
+		})
+	} else {
+		store.dispatch(logout())
+		renderApp()
+		history.push('/')
+	}
+})
